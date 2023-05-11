7ad624abe561e6909fa875f7951c30777e39a14a
Alexey Kohanovsky <awfj837@gmail.com>
2023-05-11 10:33:03 +0300

    updaet Validator

--- LR1NN/Validator.cs
+++ LR1NN/Validator.cs
@@ -2,19 +2,11 @@
 {
     public static class Validator
     {
-        public const string DATE_PATTERN = @"^(0?[1-9]|[12]\d|3[01])\.(0?[1-9]|1[012])$";
-
         public static bool IsDateCorrect(string input)
         {
-            string[] parts = input.Split('.');
-            int day, month;
-
-            if (parts.Length == 2 && int.TryParse(parts[0], out day) && int.TryParse(parts[1], out month))
+            if (DateTime.TryParse(input, out DateTime date))
             {
-                if (DateTime.TryParse($"{day}/{month}/2023", out DateTime date))
-                {
-                    return date >= DateTime.Today;
-                }
+                return date >= DateTime.Today;
             }
 
             return false;
