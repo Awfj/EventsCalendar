cdb0b238961d82dacd372cdc187631ee9f797d88
Alexey Kohanovsky <awfj837@gmail.com>
2023-05-13 08:50:05 +0300

    fix

--- LR1NN/Validator.cs
+++ LR1NN/Validator.cs
@@ -6,15 +6,9 @@
 
         public static bool IsDateCorrect(string input)
         {
-            string[] parts = input.Split('.');
-            int day, month;
-
-            if (parts.Length == 2 && int.TryParse(parts[0], out day) && int.TryParse(parts[1], out month))
+            if (DateTime.TryParse(input, out DateTime date))
             {
-                if (DateTime.TryParse($"{day}/{month}/2023", out DateTime date))
-                {
-                    return date >= DateTime.Today;
-                }
+                return date >= DateTime.Today;
             }
 
             return false;
