48caaac1088272ffd94a6547a7ad3d1955afeb6f
AWFJ <awfj837@gmail.com>
2023-05-27 15:10:31 +0300

    make Event abstract

--- LR1NN/RecurringEvent.cs
+++ LR1NN/RecurringEvent.cs
@@ -44,7 +44,7 @@
             return frequency;
         }
 
-        public new void Show()
+        public override void Show()
         {
             Console.WriteLine($"\tНазвание мероприятия: {GetName()}");
             Console.WriteLine($"\tМесто проведения: {GetPlace()}");
